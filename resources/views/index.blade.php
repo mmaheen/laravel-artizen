f3ad559d7133fc0e8b9fd14dc7e66b9c0ce57b73
mmaheen <mushrifmaheen@gmail.com>
2026-06-29 07:42:14 +0600

    Initialization

---
+++ resources/views/index.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body>
+    <div class="container mx-auto my-10 bg-gray-100">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, amet minus quidem hic, repellat deleniti
+        blanditiis voluptate, cumque temporibus nulla illo quibusdam cum doloremque illum veniam dicta animi ipsam
+        ratione!
+
+        <div class="carousel">
+            <img src="https://help.rangeme.com/hc/article_attachments/360006928633/what_makes_a_good_product_image.jpg"
+                alt="">
+            <img src="" alt="">
+        </div>
+    </div>
+</body>
+
+</html>
